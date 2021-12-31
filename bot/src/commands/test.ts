c63c9414e47558a425566495b0b175432ee17ab5
Ali Umar <ali.umar.work@gmail.com>
2021-12-31 08:16:58 -0500

    mv out

--- bot/src/commands/test.ts
+++
@@ -1,7 +0,0 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-
-const command = {
-    data: new SlashCommandBuilder().setName('ali').setDescription("This is a test command"),
-}
-
-export default command;
