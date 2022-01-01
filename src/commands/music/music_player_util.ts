dac006f0597efa5b7d30988d59462606b904f37a
Ali Umar <ali.umar.work@gmail.com>
2022-01-01 11:29:25 -0500

    proc to populate the music queue on playlist input

--- src/commands/music/music_player_util.ts
+++ src/commands/music/music_player_util.ts
@@ -1,13 +1,21 @@
 import { Message } from "discord.js"
+import { Player, Track } from "erela.js";
 
 export const musicCommandsChecks = (message: Message, args: String[]) => {
-
     // Are you in a voice Channel
     if (!message?.member?.voice?.channel) {
         message.reply("You must be in a voice channel to use this command.");
         return false;
     }
-
-
     return true;
+}
+
+
+export const populateQueue = async (message: Message, player: Player, tracks: Track[]) => {
+    const msg = await message.reply(`Adding ${tracks.length - 1} to the Queue! You will be notified when completed.`);
+
+    for (let i = 1; i < 10; ++i)
+        player.queue.add(tracks[i]);
+
+    msg.edit(`Finished adding all ${tracks.length - 1} to the Queue!`);
 }
