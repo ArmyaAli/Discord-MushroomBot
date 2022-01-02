d56e01f653889ae6ee5ab56b6fa04cdc061c6f2d
Ali Umar <ali.umar.work@gmail.com>
2022-01-02 00:00:39 -0500

    updated path

--- src/util.ts
+++ src/util.ts
@@ -4,10 +4,10 @@ import path from "path";
 // recursivly walks a directory & all subdirs 
 
 export const readFileTree = async (commands: string[], node: string): Promise<void> => {
-  const context = `${path.dirname(__filename)}\\commands`;
-  const nodeList = await fs.readdir(`${context}\\${node}`, { withFileTypes: true });
+  const context = path.join(`${path.dirname(__filename)}`, "commands");
+  const nodeList = await fs.readdir(path.join(context, node), { withFileTypes: true });
   for (const _node of nodeList) {
-    const fPath = path.join(`${__dirname}\\commands\\${node}`, _node.name);
+    const fPath = path.join(path.join(context, node), _node.name);
     if (_node.isFile()) {
       commands.push(fPath);
     } else
