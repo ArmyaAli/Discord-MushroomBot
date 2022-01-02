a4390e719560b829c1439b69275a582c61d8cbc7
Ali Umar <ali.umar.work@gmail.com>
2022-01-02 11:36:25 -0500

    fixed queue logic

--- src/commands/music/music_player_util.ts
+++ src/commands/music/music_player_util.ts
@@ -14,7 +14,7 @@ export const musicCommandsChecks = (message: Message, args: String[]) => {
 export const populateQueue = async (message: Message, player: Player, tracks: Track[]) => {
     const msg = await message.reply(`Adding ${tracks.length - 1} to the Queue! You will be notified when completed.`);
 
-    for (let i = 1; i < 10; ++i)
+    for (let i = 1; i < tracks.length; ++i)
         player.queue.add(tracks[i]);
 
     msg.edit(`Finished adding all ${tracks.length - 1} to the Queue!`);
