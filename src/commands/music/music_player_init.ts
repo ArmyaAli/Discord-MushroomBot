d56dcf9c4f8b3312b273694c5d0f51bdd9d05ae5
Ali Umar <ali.umar.work@gmail.com>
2022-01-04 00:57:25 -0500

    Sends a message on every song playing now

--- src/commands/music/music_player_init.ts
+++ src/commands/music/music_player_init.ts
@@ -1,4 +1,4 @@
-import { Message, MessageActionRow, MessageButton } from "discord.js";
+import { Message, MessageActionRow, MessageButton, TextChannel } from "discord.js";
 import { Manager, Client, Commands } from "../../config";
 
 Manager.on("nodeConnect", node => console.log(`Node ${node.options.identifier} connected`));
@@ -6,15 +6,20 @@ Manager.on("nodeConnect", node => console.log(`Node ${node.options.identifier} c
 Manager.on("nodeError", (node, error) => console.log(`Node ${node.options.identifier} had an error: ${error.message}`))
 
 Manager.on("trackStart", (player, track) => {
-    if (player.textChannel)
-        Client.channels.cache
-            .get(player.textChannel)
+    const { guild, textChannel } = player['options'];
+    const guildContext = Client.guilds.cache.get(guild);
+    if (guildContext) {
+        const textChannelContext = guildContext.channels.cache.get(textChannel) as TextChannel;
+        if (textChannelContext) {
+            textChannelContext.send(`Now playing: ${track.title} - ${track.author}. Requested by ${track?.requester}`);
+        }
+    }
+})
+
+Manager.on("trackError", (player, track) => {
+
 })
 
 Manager.on("queueEnd", (player) => {
-    if (player.textChannel)
-        Client.channels.cache
-            .get(player.textChannel)
 
-    player.destroy();
 });
