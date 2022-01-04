cad0516a3d0502c19408dd7d47e95c9a480d55c8
Ali Umar <ali.umar.work@gmail.com>
2022-01-04 00:57:59 -0500

    added ability to skip multiple songs

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -4,14 +4,18 @@ import { Manager as _Manager } from 'erela.js'
 import { Manager } from '../../config';
 
 const command = {
-    data: new SlashCommandBuilder().setName('skip').setDescription("This will skip the current song"),
-    async run(message: Message, args: String[]) {
+    data: new SlashCommandBuilder().setName('skip').setDescription("This will skip the current song. Pass in a number to skip that number of songs!"),
+    async run(message: Message, args: string[]) {
         if (!message.guild)
             return;
         // Are you in a voice Channel
         if (!message?.member?.voice?.channel) {
             message.reply("You must be in a voice channel to use this command.");
             return false;
+        }
+        if (args.length > 0 && !/\d+/.test(args[0])) {
+            message.reply("Skip command can only take a number argument!");
+            return
         }
 
         const player = Manager.create({
@@ -20,11 +24,19 @@ const command = {
             textChannel: message?.channel?.id,
         });
 
+        const songsToSkip = args[0] === undefined ? 0 : parseInt(args[0]);
+
+        if (songsToSkip > player.queue.length) {
+            message.reply("You cannot skip more songs than the Queue holds!");
+            return;
+        }
+
         if (player.queue.length <= 0) {
             message.reply("The Queue is empty.");
             return;
         }
-        message.reply(`Skipping: Now playing ${player?.stop()?.queue[0]?.title}`);
+
+        player.stop(songsToSkip)
     }
 }
 
