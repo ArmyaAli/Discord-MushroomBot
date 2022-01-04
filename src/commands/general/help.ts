07be2178758be80a2a7443e93e23ecf7083b93e3
Ali Umar <ali.umar.work@gmail.com>
2022-01-04 00:57:13 -0500

    implementedhelp command. Dynamically reads all commands

--- src/commands/general/help.ts
+++ src/commands/general/help.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { Message } from 'discord.js';
+import { Commands } from '../../config';
 
 const command = {
     data: new SlashCommandBuilder().setName('help').setDescription("Gets a list of all the commands I can use."),
@@ -7,10 +8,14 @@ const command = {
         if (!message.guild)
             return;
 
-        let msg = ``;
-
-        
-
+        const _commands = await Commands;
+        let msg = `There are total of ${_commands?.size} commands.\`\`\`\n`;
+        if (_commands) {
+            for (const [name, body] of _commands) {
+                msg += `!${name} - ${body.data.description}\n`;
+            }
+        }
+        msg+='\`\`\`';
         message.reply(msg);
     }
 }
